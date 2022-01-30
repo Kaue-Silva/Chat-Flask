fb34663c452422cbc38aa3c375bbe3e3e2766da5
kauesilva <kauesilva940@gmail.com>
2022-01-29 23:19:21 -0300

    SQLAlchemy, Flask-Migrate e Postgress

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object("config")
+
+# Apps Flask
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ from decouple import config
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", cast=bool, default=False)
+SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
